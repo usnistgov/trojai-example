418fc6384d84e193839ef129588e4e9172f75d2c
tdteach <tdteach@gmail.com>
2020-07-28 12:02:05 +0800

    bn

--- batch_run.py
+++ batch_run.py
@@ -1,12 +1,14 @@
 import os
 
 home = os.environ['HOME']
-#folder_root = os.path.join(home,'data/trojai-round0-dataset')
-folder_root = os.path.join(home,'data/round1-dataset-train/models/')
+folder_root = os.path.join(home,'data/trojai-round0-dataset')
+#folder_root = os.path.join(home,'data/round1-dataset-train/models/')
 dirs = os.listdir(folder_root)
 
 k = 0
 for d in dirs:
+  #if 'id-00000193' not in d:
+  #  continue
   model_filepath=os.path.join(folder_root, d, 'model.pt')
   examples_dirpath=os.path.join(folder_root, d, 'example_data')
 
