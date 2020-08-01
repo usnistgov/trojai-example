2deb310e970d4ea7518c56b59731375ac276cb3b
tdteach <tdteach@gmail.com>
2020-08-01 21:00:36 +0800

    v0.1

--- neuron.py
+++ neuron.py
@@ -10,7 +10,10 @@ import multiprocessing as MP
 
 
 CONSIDER_LAYER_TYPE = ['Conv2d', 'Linear']
-BATCH_SIZE = 2
+BATCH_SIZE = 32
+NUM_WORKERS = BATCH_SIZE*1
+EPS = 1e-3
+TURNPOINT_TEST_LIMIT=200
 
 
 class SingleNeuronAnalyzer:
@@ -23,33 +26,98 @@ class SingleNeuronAnalyzer:
         self.y_list = list()
 
     def _f(self, x):
+        self.x_list.append(x)
         self.pipe.send((self.n_idx, x))
-        return self.pipe.recv()
+        y = self.pipe.recv()
+        self.y_list.append(y)
+        return y
 
     def _3p_inline(self, x, y):
         lx = x[1]-x[0]
         rx = x[2]-x[1]
-        for j in range(y.shape[1]):
+        m = y[0].shape[0]
+        for j in range(m):
             ly = y[1][j]-y[0][j]
             ry = y[2][j]-y[1][j]
             cosa = (lx*ry-rx*ly)/LA.norm([lx,ly])/LA.norm([rx,ry])
-            if abs(cosa) > 0.1:
+            #print([lx, ly, rx, ry,cosa])
+            if abs(cosa) > EPS:
                 return False
         return True
 
-    def find_lower_bound(self):
-        return 0
+    def find_lowerest_turn(self, l_x, l_y, r_x, r_y):
+        if r_x < l_x+EPS:
+            return l_x, l_y
+
+        lx, ly = l_x, l_y
+        rx, ry = r_x, r_y
+        while lx+EPS < rx:
+            mx = (lx+rx)/2.0
+            my = self._f(mx)
+            if not self._3p_inline([lx,mx,rx],[ly,my,ry]):
+                rx, ry = mx, my
+            else:
+                break
+        return rx, ry
 
     def find_upper_bound(self):
-        return 10
+        delta = 1.1
+        lx, ly = self.init_x, self.init_y
+        if lx > EPS:
+            mx, my = lx*delta, self._f(lx*delta)
+        else:
+            mx, my = delta, self._f(delta)
+        while True:
+            rx, ry = mx*delta, self._f(mx*delta)
+            if not self._3p_inline([lx,mx,rx],[ly,my,ry]):
+                lx, ly = mx, my
+                mx, my = rx, ry
+            else:
+                break
+        return lx, ly
 
-    def search_trunpoints(self, l, r):
-        pass
+    def search_trunpoints(self):
+        nt_limit = TURNPOINT_TEST_LIMIT
+        nt_x = self.l_x
+        nt_y = self.l_y
+        k_nt = 0
+        rst = [nt_x]
+        while nt_x < self.r_x-EPS:
+            nt_x, nt_y = self.find_lowerest_turn(nt_x, nt_y, self.r_x, self.r_y)
+            rst.append(nt_x)
+            k_nt += 1
+            if k_nt >= nt_limit:
+                break
+        if k_nt >= nt_limit:
+            rst.append(self.r_x)
+        #print(rst)
+
+    def find_peak(self):
+        n = len(self.x_list)
+        m = self.y_list[0].shape[0]
+        peak = self.y_list[0].copy()
+        idxs = list(range(n))
+        sorted_idxs = sorted(idxs, key=lambda z: self.x_list[z])
+        #print(sorted_idxs)
+        y_list = self.y_list
+        for j in range(m):
+            peak[j] = 0
+            for i in range(1,n-1):
+                a, b, c = sorted_idxs[i-1:i+1+1]
+                ld = y_list[b][j]-y_list[a][j]
+                rd = y_list[b][j]-y_list[c][j]
+                if ld > 0 and rd > 0:
+                    peak[j] = max(peak[j],max(ld, rd))
+
+        self.peak = peak
+
 
     def run(self):
-        self.l_bound = self.find_lower_bound()
-        self.r_bound = self.find_upper_bound()
-        self.search_trunpoints(self.l_bound, self.r_bound)
+        self.l_x, self.l_y = self.find_lowerest_turn(0, self._f(0), self.init_x, self.init_y)
+        self.r_x, self.r_y = self.find_upper_bound()
+        self.search_trunpoints()
+        self.find_peak()
+        return self.peak
 
 
 class PredictingThread(threading.Thread):
@@ -92,11 +160,13 @@ class PredictingThread(threading.Thread):
             if len(x_list) == 0:
                 continue
 
+            #print(len(x_list))
+
             while True:
                 x_try = x_list[:BATCH_SIZE]
                 p_try = r_pipes[:BATCH_SIZE]
 
-                ys = self.p_func(x_list)
+                ys = self.p_func(x_try)
                 for pipe, y in zip(p_try, ys):
                     pipe.send(y)
 
@@ -257,8 +327,8 @@ class NeuronAnalyzer:
 
 
     def recall_fn(self, future):
-        idx, x_list, y_list = future.result()
-        self.results.append((idx,x_list,y_list))
+        idx, data = future.result()
+        self.results.append((idx,data))
 
 
     def analyse_children_k(self, k, init_x, init_y):
@@ -270,12 +340,12 @@ class NeuronAnalyzer:
 
         p_func = self._get_predict_function(k)
         pred_thrd = PredictingThread(p_func)
-        pipes = self.manager.list([pred_thrd.get_pipe() for _ in range(BATCH_SIZE)])
+        pipes = self.manager.list([pred_thrd.get_pipe() for _ in range(NUM_WORKERS)])
 
         pred_thrd.start()
 
-        with ProcessPoolExecutor(max_workers=2) as executor:
-            for i in range(10):
+        with ProcessPoolExecutor(max_workers=NUM_WORKERS) as executor:
+            for i in range(1000):
                 idx = rand_idx_list[i]
                 ix = init_x[idx]
                 iy = init_y
@@ -283,6 +353,7 @@ class NeuronAnalyzer:
                 ff.add_done_callback(self.recall_fn)
 
         pred_thrd.join()
+        #print(self.results)
 
 
     def analyse(self, dataloader):
@@ -296,8 +367,13 @@ class NeuronAnalyzer:
 
         layer_k = 7
         init_x = self.child[layer_k]['inputs'][0][0]
-        init_y = y
+        init_y = y[0]
         self.analyse_children_k(layer_k, init_x, init_y)
+
+        max_peaks = []
+        for z in self.results:
+            max_peaks.append(np.max(z[1]))
+        return max(max_peaks)
 
 
 def process_run(idx, init_x, init_y, pipes):
@@ -305,4 +381,4 @@ def process_run(idx, init_x, init_y, pipes):
     sna = SingleNeuronAnalyzer(idx, init_x, init_y, pipe)
     sna.run()
     pipes.append(pipe)
-    return (idx, sna.x_list, sna.y_list)
+    return (idx, sna.peak)
