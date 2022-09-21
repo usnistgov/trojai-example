79eef020976124b338485b02e8840021ced901da
tjb3 <timothy.blattner@nist.gov>
2022-09-21 12:35:51 -0400

    - Added example detector for round11

--- example_trojan_detector.py
+++ example_trojan_detector.py
@@ -11,59 +11,17 @@ import torch
 import torchvision
 import json
 import jsonschema
-import jsonpickle
 
 import logging
-import warnings
-
+import warnings 
 warnings.filterwarnings("ignore")
 
-
-def prepare_boxes(anns, image_id):
-    if len(anns) > 0:
-        boxes = []
-        class_ids = []
-        for answer in anns:
-            boxes.append(answer['bbox'])
-            class_ids.append(answer['category_id'])
-
-        class_ids = np.stack(class_ids)
-        boxes = np.stack(boxes)
-        # convert [x,y,w,h] to [x1, y1, x2, y2]
-        boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
-        boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
-    else:
-        class_ids = np.zeros((0))
-        boxes = np.zeros((0, 4))
-
-    degenerate_boxes = (boxes[:, 2:] - boxes[:, :2]) < 8
-    degenerate_boxes = np.sum(degenerate_boxes, axis=1)
-    if degenerate_boxes.any():
-        boxes = boxes[degenerate_boxes == 0, :]
-        class_ids = class_ids[degenerate_boxes == 0]
-    target = {}
-    target['boxes'] = torch.as_tensor(boxes)
-    target['labels'] = torch.as_tensor(class_ids).type(torch.int64)
-    target['image_id'] = torch.as_tensor(image_id)
-    return target
-
-
-def example_trojan_detector(model_filepath,
-                            result_filepath,
-                            scratch_dirpath,
-                            examples_dirpath,
-                            source_dataset_dirpath,
-                            round_training_dataset_dirpath,
-                            parameters_dirpath,
-                            parameter1, parameter2):
+def example_trojan_detector(model_filepath, result_filepath, scratch_dirpath, examples_dirpath, round_training_dataset_dirpath, parameters_dirpath, parameter1, parameter2, example_img_format='jpg'):
     logging.info('model_filepath = {}'.format(model_filepath))
     logging.info('result_filepath = {}'.format(result_filepath))
     logging.info('scratch_dirpath = {}'.format(scratch_dirpath))
     logging.info('examples_dirpath = {}'.format(examples_dirpath))
-    logging.info('source_dataset_dirpath = {}'.format(source_dataset_dirpath))
     logging.info('round_training_dataset_dirpath = {}'.format(round_training_dataset_dirpath))
-    logging.info('round_training_dataset_dirpath = {}'.format(round_training_dataset_dirpath))
-
     logging.info('Using parameters_dirpath = {}'.format(parameters_dirpath))
     logging.info('Using parameter1 = {}'.format(parameter1))
     logging.info('Using parameter2 = {}'.format(parameter2))
@@ -71,60 +29,54 @@ def example_trojan_detector(model_filepath,
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info("Using compute device: {}".format(device))
 
-    # load the model
-    pytorch_model = torch.load(model_filepath)
-    # move the model to the device
-    pytorch_model.to(device)
+    # load the model and move it to the GPU
+    model = torch.load(model_filepath)
+    model.to(device)
+    model.eval()
+
+    # Augmentation transformations
+    augmentation_transforms = torchvision.transforms.Compose([torchvision.transforms.ConvertImageDtype(torch.float)])
 
     # Inference the example images in data
-    fns = [os.path.join(examples_dirpath, fn) for fn in os.listdir(examples_dirpath) if fn.endswith('.jpg')]
-    fns.sort()
-    examples_filepath = fns[0]
+    fns = [os.path.join(examples_dirpath, fn) for fn in os.listdir(examples_dirpath) if fn.endswith(example_img_format)]
+    fns.sort()  # ensure file ordering
+    if len(fns) > 5: fns = fns[0:5]  # limit to 5 images
 
-    pytorch_model.eval()
+    logging.info('Inferencing {} images'.format(len(fns)))
 
-    # iterate over the example images
-    logging.info("Iterating over the examples, performing inference")
     for fn in fns:
-        image_id = os.path.basename(fn)
-        image_id = int(image_id.replace('.jpg',''))
         # load the example image
-        image = cv2.imread(fn, cv2.IMREAD_UNCHANGED)  # loads to BGR
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # convert to RGB
+        img = cv2.imread(fn, cv2.IMREAD_UNCHANGED)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        # load the annotation
-        with open(fn.replace('.jpg', '.json')) as json_file:
-            # contains a list of coco annotation dicts
-            annotations = json.load(json_file)
+        # convert the image to a tensor
+        # should be uint8 type, the conversion to float is handled later
+        image = torch.as_tensor(img)
 
-        with torch.no_grad():
-            # convert the image to a tensor
-            # should be uint8 type, the conversion to float is handled later
-            image = torch.as_tensor(image)
-            # move channels first
-            image = image.permute((2, 0, 1))
-            # convert to float (which normalizes the values)
-            image = torchvision.transforms.functional.convert_image_dtype(image, torch.float)
-            images = [image]  # wrap into list
+        # move channels first
+        image = image.permute((2, 0, 1))
 
-            # prep targets
-            targets = prepare_boxes(annotations, image_id)
-            # wrap into list
-            targets = [targets]
+        # convert to float (which normalizes the values)
+        image = augmentation_transforms(image)
+        image = image.to(device)
 
-            images = list(image.to(device) for image in images)
-            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+        # Convert to NCHW
+        image = image.unsqueeze(0)
 
-            outputs = pytorch_model(images, targets)
-            # NIST's forward function override returns both loss and boxes, so we just want the boxes here
-            outputs = outputs[1]
+        # inference
+        logits = model(image).cpu().detach().numpy()
+
+        pred = np.argmax(logits)
+        logging.info('example img filepath = {}, logits = {}, pred: = {}'.format(fn, logits, pred))
+
 
     # Test scratch space
-    with open(os.path.join(scratch_dirpath, 'test.txt'), 'w') as fh:
-        fh.write('this is a test')
+    img = np.random.rand(1, 3, 224, 224)
+    img_tmp_fp = os.path.join(scratch_dirpath, 'img')
+    np.save(img_tmp_fp, img)
 
     trojan_probability = np.random.rand()
-    logging.info('Trojan Probability: {}'.format(trojan_probability))
+    print('Trojan Probability: {}'.format(trojan_probability))
 
     with open(result_filepath, 'w') as fh:
         fh.write("{}".format(trojan_probability))
@@ -160,19 +112,17 @@ def configure(source_dataset_dirpath,
     example_dict['keyg'] = 17
 
     with open(os.path.join(output_parameters_dirpath, "dict.json"), mode='w', encoding='utf-8') as f:
-        f.write(jsonpickle.encode(example_dict, warn=True, indent=2))
+        json.dump(example_dict, f, warnings=True, indent=2)
 
 
 if __name__ == "__main__":
     from jsonargparse import ArgumentParser, ActionConfigFile
 
     parser = ArgumentParser(description='Fake Trojan Detector to Demonstrate Test and Evaluation Infrastructure.')
-    parser.add_argument('--model_filepath', type=str, help='File path to the pytorch model file to be evaluated.')
-    parser.add_argument('--result_filepath', type=str, help='File path to the file where output result should be written. After execution this file should contain a single line with a single floating point trojan probability.')
-    parser.add_argument('--scratch_dirpath', type=str, help='File path to the folder where scratch disk space exists. This folder will be empty at execution start and will be deleted at completion of execution.')
-    parser.add_argument('--examples_dirpath', type=str, help='File path to the directory containing json file(s) that contains the examples which might be useful for determining whether a model is poisoned.')
-
-    parser.add_argument('--source_dataset_dirpath', type=str, help='File path to a directory containing the original clean dataset into which triggers were injected during training.', default=None)
+    parser.add_argument('--model_filepath', type=str, help='File path to the pytorch model file to be evaluated.', default='./model.pt')
+    parser.add_argument('--result_filepath', type=str, help='File path to the file where output result should be written. After execution this file should contain a single line with a single floating point trojan probability.', default='./output')
+    parser.add_argument('--scratch_dirpath', type=str, help='File path to the folder where scratch disk space exists. This folder will be empty at execution start and will be deleted at completion of execution.', default='./scratch')
+    parser.add_argument('--examples_dirpath', type=str, help='File path to the folder of examples which might be useful for determining whether a model is poisoned.', default='./example')
     parser.add_argument('--round_training_dataset_dirpath', type=str, help='File path to the directory containing id-xxxxxxxx models of the current rounds training dataset.', default=None)
 
     parser.add_argument('--metaparameters_filepath', help='Path to JSON file containing values of tunable paramaters to be used when evaluating models.', action=ActionConfigFile)
@@ -192,27 +142,30 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s")
     logging.info("example_trojan_detector.py launched")
-    logging.info(args)
 
     # Validate config file against schema
     config_json = None
     if args.metaparameters_filepath is not None:
-        if args.schema_filepath is not None:
-            with open(args.metaparameters_filepath[0]()) as config_file:
-                config_json = json.load(config_file)
+        with open(args.metaparameters_filepath[0]()) as config_file:
+            config_json = json.load(config_file)
+            if args.parameter1 is None:
+                args.parameter1 = config_json['parameters1']
+            if args.parameter2 is None:
+                args.parameter2 = config_json['parameters1']
+    if args.schema_filepath is not None:
+        with open(args.schema_filepath) as schema_file:
+            schema_json = json.load(schema_file)
 
-            with open(args.schema_filepath) as schema_file:
-                schema_json = json.load(schema_file)
+        # this throws a fairly descriptive error if validation fails
+        jsonschema.validate(instance=config_json, schema=schema_json)
 
-            # this throws a fairly descriptive error if validation fails
-            jsonschema.validate(instance=config_json, schema=schema_json)
+    logging.info(args)
 
     if not args.configure_mode:
         if (args.model_filepath is not None and
             args.result_filepath is not None and
             args.scratch_dirpath is not None and
             args.examples_dirpath is not None and
-            args.source_dataset_dirpath is not None and
             args.round_training_dataset_dirpath is not None and
             args.learned_parameters_dirpath is not None and
             args.parameter1 is not None and
@@ -223,7 +176,6 @@ if __name__ == "__main__":
                                     args.result_filepath,
                                     args.scratch_dirpath,
                                     args.examples_dirpath,
-                                    args.source_dataset_dirpath,
                                     args.round_training_dataset_dirpath,
                                     args.learned_parameters_dirpath,
                                     args.parameter1, args.parameter2)
@@ -243,3 +195,5 @@ if __name__ == "__main__":
                       args.parameter3)
         else:
             logging.info("Required Configure-Mode parameters missing!")
+
+
