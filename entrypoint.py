7b367880ca74a08672a59689ce1dc26aed010316
Michael Majurski <mmajurski@gmail.com>
2023-03-02 09:50:32 -0500

    added NIST code dislaimer

--- entrypoint.py
+++ entrypoint.py
@@ -1,3 +1,9 @@
+# NIST-developed software is provided by NIST as a public service. You may use, copy and distribute copies of the software in any medium, provided that you keep intact this entire notice. You may improve, modify and create derivative works of the software or any portion of the software, and you may copy and distribute such modifications or works. Modified works should carry a notice stating that you changed the software and should note the date and nature of any such change. Please explicitly acknowledge the National Institute of Standards and Technology as the source of the software.
+
+# NIST-developed software is expressly provided "AS IS." NIST MAKES NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY OPERATION OF LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT AND DATA ACCURACY. NIST NEITHER REPRESENTS NOR WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE UNINTERRUPTED OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. NIST DOES NOT WARRANT OR MAKE ANY REPRESENTATIONS REGARDING THE USE OF THE SOFTWARE OR THE RESULTS THEREOF, INCLUDING BUT NOT LIMITED TO THE CORRECTNESS, ACCURACY, RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
+
+# You are solely responsible for determining the appropriateness of using and distributing the software and you assume all risks associated with its use, including but not limited to the risks and costs of program errors, compliance with applicable laws, damage to or loss of data, programs or equipment, and the unavailability or interruption of operation. This software is not intended to be used in any situation where a failure could cause risk of injury or damage to property. The software developed by NIST employees is not subject to copyright protection within the United States.
+
 """ Entrypoint to interact with the detector.
 """
 import json
@@ -9,6 +15,7 @@ import jsonschema
 from detector import Detector
 
 warnings.filterwarnings("ignore")
+
 
 def inference_mode(args):
     # Validate config file against schema
@@ -25,6 +32,8 @@ def inference_mode(args):
 
     logging.info("Calling the trojan detector")
     detector.infer(args.model_filepath, args.result_filepath, args.scratch_dirpath, args.examples_dirpath, args.round_training_dataset_dirpath)
+
+
 def configure_mode(args):
     # Validate config file against schema
     with open(args.metaparameters_filepath) as config_file:
@@ -40,6 +49,7 @@ def configure_mode(args):
 
     logging.info("Calling configuration mode")
     detector.configure(args.configure_models_dirpath, args.automatic_configuration)
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -118,7 +128,6 @@ if __name__ == "__main__":
     )
 
     inf_parser.set_defaults(func=inference_mode)
-
 
     configure_parser = subparser.add_parser('configure', help='Execute container in configuration mode for TrojAI detection. This will produce a new set of learned parameters to be used in inference mode.')
 
