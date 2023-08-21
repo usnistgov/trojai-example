80a8899057185e56579db05f3ae89ee942f99966
tjb3 <timothy.blattner@nist.gov>
2023-08-21 18:36:36 -0400

    - Added architectures and world building

--- utils/models.py
+++ utils/models.py
@@ -1,5 +1,5 @@
 # NIST-developed software is provided by NIST as a public service. You may use, copy and distribute copies of the software in any medium, provided that you keep intact this entire notice. You may improve, modify and create derivative works of the software or any portion of the software, and you may copy and distribute such modifications or works. Modified works should carry a notice stating that you changed the software and should note the date and nature of any such change. Please explicitly acknowledge the National Institute of Standards and Technology as the source of the software.
-
+import math
 # NIST-developed software is expressly provided "AS IS." NIST MAKES NO WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY OPERATION OF LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT AND DATA ACCURACY. NIST NEITHER REPRESENTS NOR WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE UNINTERRUPTED OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. NIST DOES NOT WARRANT OR MAKE ANY REPRESENTATIONS REGARDING THE USE OF THE SOFTWARE OR THE RESULTS THEREOF, INCLUDING BUT NOT LIMITED TO THE CORRECTNESS, ACCURACY, RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
 
 # You are solely responsible for determining the appropriateness of using and distributing the software and you assume all risks associated with its use, including but not limited to the risks and costs of program errors, compliance with applicable laws, damage to or loss of data, programs or equipment, and the unavailability or interruption of operation. This software is not intended to be used in any situation where a failure could cause risk of injury or damage to property. The software developed by NIST employees is not subject to copyright protection within the United States.
@@ -11,6 +11,266 @@ from os.path import join
 
 import torch
 from tqdm import tqdm
+
+import gymnasium as gym
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision.models.resnet import BasicBlock
+
+def linear_w_relu(dims: list, end_relu=True):
+    """Helper function for creating sequential linear layers"""
+    layers = []
+    for i in range(len(dims) - 1):
+        layers.append(nn.Linear(dims[i], dims[i + 1]))
+        layers.append(nn.ReLU())
+    if not end_relu:
+        del layers[-1]
+    return nn.Sequential(*layers)
+
+
+class ModdedResnet18(torchvision.models.ResNet):
+    """Modified ResNet18 architecture for TrojAI DRL ResNet models"""
+    def __init__(self):
+        super(ModdedResnet18, self).__init__(BasicBlock, [2, 2, 2, 2])
+
+    def _forward_impl(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        return x
+
+
+class TrojaiDRLBackbone(nn.Module):
+    """Base class for TrojAI DRL models"""
+
+    def __init__(self, embedding, actor, critic):
+        nn.Module.__init__(self)
+        self.state_emb = embedding  # Define state embedding
+        self.actor = actor  # Define actor's model
+        self.critic = critic  # Define critic's model
+        self.value = 0
+
+    def forward(self, obs):
+        agent_dir = obs['direction'].long()
+        obs = obs['image']
+        x = self.state_emb(obs.float())
+        x = x.reshape(x.shape[0], -1)
+        x = torch.concat([x, agent_dir], dim=1)
+        x_act = self.actor(x)
+        x_crit = self.critic(x)
+        self.value = x_crit.squeeze(1)
+        return x_act
+
+    def value_function(self):
+        return self.value
+
+    def args_dict(self):
+        raise NotImplementedError("Should be implemented in subclass")
+
+
+class FCModel(TrojaiDRLBackbone):
+    """Fully-connected actor-critic model with shared embedding"""
+
+    def __init__(self, obs_space, action_space, linear_embedding_dims=(512, 256), actor_linear_mid_dims=(64, 32),
+                 critic_linear_mid_dims=(64, 32)):
+        """
+        Initialize the model.
+        :param obs_space: (gym.Spaces) Observation space of the environment being used for training.
+        :param action_space: (gym.Spaces) Action space of the environment being used for training. Used to determine
+            the size of the actor's output later.
+        :param linear_embedding_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the linear embedding
+        :param actor_linear_mid_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the actor network
+        :param critic_linear_mid_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the critic network
+        """
+        self.obs_space = obs_space
+        if isinstance(self.obs_space, gym.spaces.Dict):
+            flattened_dims = int(math.prod(self.obs_space['image'].shape))
+        else:
+            flattened_dims = int(math.prod(self.obs_space.shape))
+        self.action_space = action_space
+        self.linear_embedding_dims = linear_embedding_dims
+        self.actor_linear_mid_dims = actor_linear_mid_dims
+        self.critic_linear_mid_dims = critic_linear_mid_dims
+
+        # +1 because we concat direction information to embedding
+        self.state_embedding_size = linear_embedding_dims[-1] + 1
+        embedding = linear_w_relu([flattened_dims] + [d for d in linear_embedding_dims])
+        embedding.insert(0, nn.Flatten())  # put a flattening layer in front
+        actor_dims = [self.state_embedding_size] + list(actor_linear_mid_dims) + [action_space.n]
+        critic_dims = [self.state_embedding_size] + list(critic_linear_mid_dims) + [1]
+        super().__init__(
+            embedding,
+            linear_w_relu(actor_dims, end_relu=False),
+            linear_w_relu(critic_dims, end_relu=False)
+        )
+
+    def args_dict(self):
+        return {
+            'obs_space': self.obs_space,
+            'action_space': self.action_space,
+            'linear_embedding_dims': self.linear_embedding_dims,
+            'actor_linear_mid_dims': self.actor_linear_mid_dims,
+            'critic_linear_mid_dims': self.critic_linear_mid_dims
+        }
+
+
+class CNNModel(TrojaiDRLBackbone):
+    """Simple actor-critic model with CNN embedding"""
+
+    def __init__(self, obs_space, action_space, channels=(16, 32, 64), actor_linear_mid_dims=(144,),
+                 critic_linear_mid_dims=(144,)):
+        """
+        Initialize the model.
+        :param obs_space: (gym.Spaces) Observation space of the environment being used for training.
+        :param action_space: (gym.Spaces) Action space of the environment being used for training. Used to determine
+            the size of the actor's output later.
+        :param channels: (iterable) Sequence of 3 integers representing the number of numbers of channels to use for the
+            CNN embedding
+        :param actor_linear_mid_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the actor network
+        :param critic_linear_mid_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the critic network
+        """
+        if len(channels) != 3:
+            raise ValueError("'channels' must be a tuple or list of length 3")
+
+        self.obs_space = obs_space
+        self.action_space = action_space
+        self.channels = channels
+        self.actor_linear_mid_dims = actor_linear_mid_dims
+        self.critic_linear_mid_dims = critic_linear_mid_dims
+
+        c1, c2, c3 = channels
+        image_embedding_size = 4 * 4 * c3 + 1  # +1 because we concat direction information to embedding
+        image_conv = nn.Sequential(
+            nn.Conv2d(3, c1, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(c1, c2, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(c2, c3, (2, 2)),
+            nn.ReLU()
+        )
+        actor_dims = [image_embedding_size] + list(actor_linear_mid_dims) + [action_space.n]
+        critic_dims = [image_embedding_size] + list(critic_linear_mid_dims) + [1]
+        super().__init__(
+            image_conv,
+            linear_w_relu(actor_dims, end_relu=False),
+            linear_w_relu(critic_dims, end_relu=False)
+        )
+
+    def args_dict(self):
+        return {
+            'obs_space': self.obs_space,
+            'action_space': self.action_space,
+            'channels': self.channels,
+            'actor_linear_mid_dims': self.actor_linear_mid_dims,
+            'critic_linear_mid_dims': self.critic_linear_mid_dims
+        }
+
+
+class ImageACModel(TrojaiDRLBackbone):
+    """Simple CNN Actor-Critic model designed for MiniGrid. Assumes 48x48 grayscale or RGB images."""
+
+    def __init__(self, obs_space, action_space, channels=(8, 16, 32), actor_linear_mid_dims=(144,),
+                 critic_linear_mid_dims=(144,)):
+        """
+        Initialize the model.
+        :param obs_space: (gym.Spaces) Observation space of the environment being used for training. Technically unused
+            for this model, but stored both for consistency between models and to be used for later reference if needed.
+        :param action_space: (gym.Spaces) Action space of the environment being used for training. Used to determine
+            the size of the actor's output later.
+        :param channels: (iterable) Sequence of 3 integers representing the number of numbers of channels to use for the
+            CNN embedding
+        :param actor_linear_mid_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the actor network
+        :param critic_linear_mid_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the critic network
+        """
+
+        self.obs_space = obs_space
+        self.action_space = action_space
+        self.channels = channels
+        self.actor_linear_mid_dims = actor_linear_mid_dims
+        self.critic_linear_mid_dims = critic_linear_mid_dims
+        self.image_size = 48  # this is the size of image this CNN was designed for
+
+        num_channels = obs_space['image'].shape[0]
+        c1, c2, c3 = channels
+        image_embedding_size = 3 * 3 * c3 + 1  # +1 because we concat direction information to embedding
+        image_conv = nn.Sequential(
+            nn.Conv2d(num_channels, c1, (3, 3), stride=3),
+            nn.ReLU(),
+            nn.Conv2d(c1, c2, (4, 4), stride=2),
+            nn.ReLU(),
+            nn.Conv2d(c2, c3, (3, 3), stride=2),
+            nn.ReLU()
+        )
+        actor_dims = [image_embedding_size] + list(actor_linear_mid_dims) + [action_space.n]
+        critic_dims = [image_embedding_size] + list(critic_linear_mid_dims) + [1]
+        super().__init__(
+            image_conv,
+            linear_w_relu(actor_dims, end_relu=False),
+            linear_w_relu(critic_dims, end_relu=False)
+        )
+
+    def args_dict(self):
+        return {
+            'obs_space': self.obs_space,
+            'action_space': self.action_space,
+            'channels': self.channels,
+            'actor_linear_mid_dims': self.actor_linear_mid_dims,
+            'critic_linear_mid_dims': self.critic_linear_mid_dims
+        }
+
+
+class ResNetACModel(TrojaiDRLBackbone):
+    """Actor-Critic model with ResNet18 embedding designed for MiniGrid. Assumes 112x112 RGB images."""
+
+    def __init__(self, obs_space, action_space, actor_linear_mid_dims=(512,), critic_linear_mid_dims=(512,)):
+        """
+        Initialize the model.
+        :param obs_space: (gym.Spaces) Observation space of the environment being used for training. Technically unused
+            for this model, but stored both for consistency between models and to be used for later reference if needed.
+        :param action_space: (gym.Spaces) Action space of the environment being used for training. Used to determine
+            the size of the actor's output later.
+        :param actor_linear_mid_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the actor network
+        :param critic_linear_mid_dims: (iterable) Sequence of integers representing the number of hidden nodes in the
+            intermediate layers of the critic network
+        """
+        self.obs_space = obs_space
+        self.action_space = action_space
+        self.actor_linear_mid_dims = actor_linear_mid_dims
+        self.critic_linear_mid_dims = critic_linear_mid_dims
+
+        image_embedding_size = 512 + 1  # +1 because we concat direction information to embedding
+        embedding = ModdedResnet18()
+        actor_dims = [image_embedding_size] + list(actor_linear_mid_dims) + [action_space.n]
+        critic_dims = [image_embedding_size] + list(critic_linear_mid_dims) + [1]
+        super().__init__(
+            embedding,
+            linear_w_relu(actor_dims, end_relu=False),
+            linear_w_relu(critic_dims, end_relu=False)
+        )
+
+    def args_dict(self):
+        return {
+            'obs_space': self.obs_space,
+            'action_space': self.action_space,
+            'actor_linear_mid_dims': self.actor_linear_mid_dims,
+            'critic_linear_mid_dims': self.critic_linear_mid_dims
+        }
 
 
 def create_layer_map(model_repr_dict):
@@ -46,8 +306,16 @@ def load_model(model_filepath: str) -> (dict, str):
     Returns:
         model, dict, str - Torch model + dictionary representation of the model + model class name
     """
-    model = torch.load(model_filepath)
-    model_class = model.__class__.__name__
+
+    stored_dict = torch.load(model_filepath, map_location='cuda' if torch.cuda.is_available() else 'cpu')
+    model_name = stored_dict.pop('model')
+    if model_name not in [FCModel.__name__, CNNModel.__name__, ImageACModel.__name__, ResNetACModel.__name__]:
+        raise ValueError(f"Unknown model name: {model_name} extracted from model dict")
+    model_class = eval(model_name)
+    state_dict = stored_dict.pop('state_dict')
+    model = model_class(**stored_dict)
+    model.load_state_dict(state_dict)
+
     model_repr = OrderedDict(
         {layer: tensor.numpy() for (layer, tensor) in model.state_dict().items()}
     )
---
+++ utils/world.py
@@ -0,0 +1,222 @@
+import itertools
+import random
+import gymnasium as gym
+
+from gym.utils import seeding
+from minigrid.minigrid_env import MiniGridEnv, MissionSpace, TILE_PIXELS
+from minigrid.core.grid import Grid
+from minigrid.core.world_object import Lava, Goal
+
+import numpy as np
+
+ALLOWED_LAVA_COVERAGE = 0.6  # fraction/percent of grid that can be lava
+DEFAULT_GRID_SIZE = 9
+
+class RandomLavaWorldEnv(MiniGridEnv):
+    def __init__(self, grid_size=9, width=None, height=None, max_steps=250, agent_view_size=7, mode='simple'):
+        self.py_random = None
+        self.np_random = None
+        self.mode = mode
+        self.obstacle_type = Lava
+        self.agent_last_pos = None
+        self.step_count = None
+        self.carrying = None
+        self.allowed_lava_coverage = ALLOWED_LAVA_COVERAGE
+        self.max_rand_tries = 100
+        self.num_lava_squares = 0
+        self.max_lava_squares = 0
+
+        super().__init__(mission_space=MissionSpace(lambda: "avoid the lava and get to the green goal square"),
+                        grid_size=grid_size,
+                        width=width,
+                        height=height,
+                        max_steps=max_steps,
+                        see_through_walls=True,
+                        agent_view_size=agent_view_size)
+
+        self.reset()
+        self._set_env_attributes()
+
+    def seed(self, seed=None):
+        self.np_random, _ = seeding.np_random(seed)
+        self.py_random = random.Random(seed)
+
+    def _set_env_attributes(self):
+        self.action_space = gym.spaces.Discrete(3)
+
+        playable_grid_tiles = (self.width - 1) * (self.height - 1)
+        self.max_lava_squares = int(self.allowed_lava_coverage * playable_grid_tiles)
+
+        if self.mode == 'simple':
+            obs_shape = (self.agent_view_size, self.agent_view_size, 3)
+        else:
+            obs_shape = (self.agent_view_size * TILE_PIXELS, self.agent_view_size * TILE_PIXELS, 3)
+
+        self.observation_space = gym.spaces.Dict({'image': gym.spaces.Box(0, 255, shape=obs_shape, dtype=np.uint8),
+                                                  'direction': gym.spaces.Box(0, 3, (1,), dtype=int)})
+
+    def _available_pos_set(self):
+        """
+        Create set of positions within the grid where objects can be placed.
+        :return: (set) Set of 2-tuples of allowed grid positions for objects.
+        """
+        return set(itertools.product(range(1, self.width - 1), range(1, self.height - 1)))
+
+    def _rand_pos_in_grid(self):
+        """
+        Return a random position within the grid walls
+        """
+        return self._rand_pos(1, self.width - 1, 1, self.height - 1)
+
+    def _get_image_obs(self):
+        return self.get_pov_render(TILE_PIXELS)
+
+    def no_state_put_obj(self, grid, obj, i, j):
+        grid.set(i, j, obj)
+        obj.init_pos = (i, j)
+        obj.cur_pos = (i, j)
+
+    def _dfs_iter(self, mg_grid, visited, x, y):
+        """
+        Depth first search based check that there exists a path to the goal.
+        :param mg_grid: (Grid) The MiniGrid Grid object
+        :param visited: (numpy array) 2d grid of visited grid locations, value of 2 means "visited".
+        :param x: (int) Grid x coordinate to visit on this iteration
+        :param y: (int) Grid y coordinate to visit on this iteration
+        :return: (bool) If path to goal was discovered.
+        """
+        nbs = []
+        visited[y, x] = 1  # grid coordinates are backwards, shouldn't matter in general, but helps when debugging
+        for i, j in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            new_x, new_y = x + i, y + j
+            cell = mg_grid.get(new_x, new_y)
+            if cell:
+                if cell.type == 'goal':
+                    return True
+                else:
+                    visited[new_y, new_x] = 2
+            elif visited[new_y, new_x]:
+                pass
+            else:
+                nbs.append((new_x, new_y))
+        if len(nbs) > 0:
+            return any(self._dfs_iter(mg_grid, visited, n[0], n[1]) for n in nbs)
+        else:
+            return False
+
+    def _path_exists(self, grid):
+        """
+        Assure that there exists a path in the grid from the first location to the second, assuming a lavaworld grid
+        :param grid (Grid) MiniGrid Grid object to check.
+        """
+        visit_grid = np.zeros((grid.height, grid.width), dtype=np.int8)
+        visit_grid[:, 0] = 2
+        visit_grid[:, -1] = 2
+        visit_grid[0, :] = 2
+        visit_grid[-1, :] = 2
+        # Note: grid y axis is inverted, but shouldn't matter for this application
+
+        return self._dfs_iter(grid, visit_grid, 1, 1)
+
+    def _gen_grid(self, width, height):
+        """
+        Try to set the grid using current parameters.
+        :param width (int) Width of the grid.
+        :param height (int) Height of the grid.
+        :return: None
+        """
+        assert width % 2 == 1 and height % 2 == 1  # odd size
+
+        # Create an empty grid
+        grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        grid.wall_rect(0, 0, width, height)
+
+        available_pos = self._available_pos_set()
+
+        # Place the agent in the top-left corner
+        self.agent_pos = (1, 1)
+        self.agent_dir = 0
+        available_pos.remove(self.agent_pos)
+
+        # Place a goal square in the bottom-right corner
+        goal_pos = (width - 2, height - 2)
+        self.no_state_put_obj(grid, Goal(), *goal_pos)
+        available_pos.remove(goal_pos)
+
+        # place random lava squares
+        n_lava_squares = self._rand_int(self.num_lava_squares, self.max_lava_squares)
+
+        num_placed_lava_squares = 0
+        while num_placed_lava_squares < n_lava_squares:
+            # get random square within grid (not including outside walls)
+            rand_pos = self.py_random.sample(available_pos, 1)[0]
+            self.no_state_put_obj(grid, self.obstacle_type(), *rand_pos)
+            available_pos.remove(rand_pos)
+            num_placed_lava_squares += 1
+
+        self.grid = grid
+
+        self.mission = (
+            "avoid the lava and get to the green goal square"
+            if self.obstacle_type == Lava
+            else "find the opening and get to the green goal square"
+        )
+
+    def reset(self, *, seed=None, options=None):
+        super().reset(seed=seed)
+        self.seed(seed)
+        self._set_env_attributes()
+
+        self.agent_last_pos = None
+        self.agent_pos = None
+        self.agent_dir = None
+        self.step_count = 0
+
+        valid_grid = False
+
+        if not valid_grid:
+            self._gen_grid(self.width, self.height)
+            tries = 0
+            while not self._path_exists(self.grid):
+                self._gen_grid(self.width, self.height)
+                tries += 1
+                if tries > self.max_rand_tries:
+                    raise RuntimeError("Could not generate valid environment in {} tries. Check: \n"
+                                       "- if __init__ or reset arguments prevent valid environment generation\n"                                      
+                                       "- if increasing self.max_rand_tries helps".format(self.max_rand_tries))
+
+        assert self.agent_pos is not None
+        assert self.agent_dir is not None
+
+        start_cell = self.grid.get(*self.agent_pos)
+        assert start_cell is None or start_cell.can_overlap()
+
+        obs = self.gen_obs()
+        obs.pop('mission')
+        obs['direction'] = np.array(obs['direction']).reshape((1,))
+        if self.mode == 'rgb':
+            obs['image'] = self._get_image_obs()
+
+        if self.window:
+            self.window.close()
+            self.window = None
+
+        return obs, {}
+
+    def step(self, action: int):
+        if action not in self.action_space:
+            if not (isinstance(action, np.ndarray) and action[0] in self.action_space):
+                raise RuntimeError("Received action outside of action space! action={}".format(action))
+
+        self.agent_last_pos = self.agent_pos
+        obs, reward, terminated, truncated, info = MiniGridEnv.step(self, action)
+
+        obs['direction'] = np.array(obs['direction']).reshape((1,))
+        obs.pop('mission')
+
+        if self.mode == 'rgb':
+            obs['image'] = self._get_image_obs()
+
+        return obs, reward, terminated, truncated, info
