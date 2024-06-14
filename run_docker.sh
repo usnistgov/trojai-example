4b9ad7fc03b59cdc511b6dc5c3a46bf0aaeb60a4
tjb3 <timothy.blattner@nist.gov>
2024-06-14 13:02:17 -0400

    - Removed unneeded files and fixed issue in inference

--- run_docker.sh
+++
@@ -1,9 +0,0 @@
-docker run --gpus 0 -it \
--v /home/carnejk1/mitigation_data:/trojai-example/data/:ro \
--v /home/carnejk1/trojai-example-mitigation/out:/trojai-example/output/:rw \
-mitigation-test \
---test \
---model_filepath /trojai-example/data/mitigated_model.pt \
---dataset /trojai-example/data/dataset/clean_testset \
---output_dirpath /trojai-example/output \
---num_classes 10
