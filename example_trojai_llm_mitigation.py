1f7b878b2c0c57cecf3412c326270603821f53b9
carnejk1 <Joshua.Carney@jhuapl.edu>
2024-08-05 15:02:57 -0400

    removed comments

--- example_trojai_llm_mitigation.py
+++ example_trojai_llm_mitigation.py
@@ -119,39 +119,6 @@ def run_test_mode(args):
 
 
 if __name__ == "__main__":
-    # import configargparse
-    # parser = configargparse.ArgParser(
-    #     config_file_parser_class=configargparse.YAMLConfigFileParser
-    # )
-
-    # parser.add_argument(
-    #     "--metaparameters",
-    #     is_config_file_arg=True,
-    #     type=str,
-    #     required=True,
-    #     help="Required metaparameters YAML file"
-    # )
-
-    # parser.add_argument("--model", type=str, help="The model we are conducting mitigation or testing on")
-    # parser.add_argument("--mitigate", action='store_true', help="Pass this flag if you want to conduct your mitigation technique on a saved model")
-    # parser.add_argument("--test", action='store_true', help="Pass this flag in if you want to do test/inference on the model")
-
-    # parser.add_argument("--dataset", type=str, default=None, help="The dataset either given to us during mitigation (if at all), or if we are conducting testing, the dataset we will test on")
-    # parser.add_argument('--scratch_dirpath', type=str, default="./scratch", help="File path to the folder where a scratch space is located.")
-    # parser.add_argument('--output_dirpath', type=str, default="./out", help="File path to where the output model will be dumped")
-
-    # parser.add_argument("--lora_parameters", type=yaml.safe_load, help="Dummy CLI arg for the lora config parameters from the metaparameters.yml file. Not designed to be passed in directly")
-    # parser.add_argument("--model_parameters", type=yaml.safe_load, help="Dummy CLI arg for the model config parameters from the metaparameters.yml file. Not designed to be passed in directly")
-    
-    # # Example performer specific args
-    # parser.add_argument("--learning_rate", type=float, help="Learning rate to use for fine tuning")
-    # parser.add_argument("--num_train_epochs", type=float, help="Number of epochs to run fine tuning for")
-    # parser.add_argument("--optim", type=str, help="Optimizer to use for fine tuning")
-
-    # parser.add_argument('--batch_size', type=int, default=4, help="The batch size that the technique would use for dataloading")
-    # parser.add_argument('--device', type=str, default='cuda', help="The device to use")
-    # parser.add_argument('--num_workers', type=int, default=1, help="The number of CPU processes to use to load data")
-    # parser.add_argument('--bf16', action='store_true', help="Whether or not to use bf16")
     from argparse import ArgumentParser
     
     parser = ArgumentParser(description='Parser for the LLM mitigation round with two modes of operation, mitigate and test')
@@ -194,25 +161,3 @@ if __name__ == "__main__":
 
     # Call appropriate function
     args.func(args)
-        
-    # args = parser.parse_args()
-    # model, tokenizer = prepare_model_and_tokenizer(args.model, args.model_parameters, args.device)
-    # peft_config = prepare_peft(args.lora_parameters)
-    # dataset = prepare_dataset(args.dataset)
-    # print("Finished prepping dataset")
-    # print_gpu_utilization()
-    # collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
-
-    # if args.mitigate:
-    #     mitigation = prepare_mitigation(args)
-    #     mitigated_model = mitigation.mitigate_model(
-    #         model=model,
-    #         collator=collator,
-    #         peft_config=peft_config,
-    #         dataset=dataset
-    #     )
-    #     mitigated_model.save_pretrained(args.output_dirpath)
-
-    # # TODO: inferencing pipeline
-    # if args.test:
-    #     pass
