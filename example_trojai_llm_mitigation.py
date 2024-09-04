b5721655a28a1e309e330266ed5912a719534592
fendlnm1 <Neil.Fendley@jhuapl.edu>
2024-09-04 12:16:49 -0400

    changing to use new collate function and work with lm_eval library

--- example_trojai_llm_mitigation.py
+++ example_trojai_llm_mitigation.py
@@ -5,7 +5,7 @@ import jsonschema
 import json
 from datasets import load_dataset, Dataset
 import yaml
-from trl import setup_chat_format
+from trl import setup_chat_format, DataCollatorForCompletionOnlyLM
 
 from trojai_llm_mitigation_round.mitigations.finetuning import FineTuningTrojaiMitigationLLM
 from trojai_llm_mitigation_round.utils import print_gpu_utilization
@@ -13,17 +13,37 @@ from trojai_llm_mitigation_round.utils import print_gpu_utilization
 
 TASK_TYPE = TaskType.CAUSAL_LM
 AUTO_MODEL_CLS = AutoModelForCausalLM
+INSTRUCTION_TEMPLATE_LOOKUP = {
+        'google/gemma-2-2b-it': '<start_of_turn>user',
+        'google/gemma-2-9b-it': '<start_of_turn>user',
+        'google/gemma-2b-it': '<start_of_turn>user',
+        'google/gemma-7b-it': '<start_of_turn>user',
+        'meta-llama/Llama-2-7b-chat-hf': '<s>[INST]',
+        'meta-llama/Meta-Llama-3-8B-Instruct': '<|begin_of_text|><|start_header_id|>user<|end_header_id|>',
+        "mistralai/Mixtral-8x7B-Instruct-v0.1": '[INST]',
+        'meta-llama/Meta-Llama-3.1-8B-Instruct': '<|begin_of_text|><|start_header_id|>user<|end_header_id|>'
+    }
+RESPONSE_TEMPLATE_LOOKUP = {
+    'google/gemma-2-2b-it': '<start_of_turn>model\n',
+    'google/gemma-2-9b-it': '<start_of_turn>model\n',
+    'google/gemma-2b-it': '<start_of_turn>model\n',
+    'google/gemma-7b-it': '<start_of_turn>model\n',
+    'meta-llama/Llama-2-7b-chat-hf': '[/INST]',
+    'meta-llama/Meta-Llama-3-8B-Instruct': '<|start_header_id|>assistant<|end_header_id|>',
+    "mistralai/Mixtral-8x7B-Instruct-v0.1": '[/INST]',
+    'meta-llama/Meta-Llama-3.1-8B-Instruct': '<|begin_of_text|><|start_header_id|>assistant<|end_header_id|>'
+}
 
-
-def prepare_mitigation(config_json, args):
+def prepare_mitigation(config_json, argv):
     mitigation = FineTuningTrojaiMitigationLLM(
         lr=config_json['learning_rate'],
         train_epochs=config_json['num_train_epochs'],
         optim=config_json['optim'],
-        device=args.device,
+        device=argv.device,
         batch_size=config_json['batch_size'],
-        num_workers=args.num_workers,
+        num_workers=argv.num_workers,
         bf16=config_json['bf16'],
+        max_token_length=config_json['max_token_length'],
     )
     return mitigation
 
@@ -34,9 +54,8 @@ def prepare_dataset(dataset_path, split='train'):
     dataset = dataset.train_test_split(test_size=0.2)
     
     # Note: Debug hack to make the dataset smaller for debugging
-    dataset['train'] = Dataset.from_dict(dataset['train'][:1000])
+    dataset['train'] = Dataset.from_dict(dataset['train'][:10000])
     dataset['test'] = Dataset.from_dict(dataset['test'][:1000])
-
     return dataset
 
 
@@ -44,7 +63,7 @@ def prepare_peft(lora_parameters):
     return LoraConfig(task_type=TASK_TYPE, **lora_parameters)
 
 
-def prepare_model_and_tokenizer(model_path, model_params, device):
+def prepare_model_and_tokenizer(model_path, model_params):
     if model_params['model_dtype'] == 'float16':
         dtype = torch.float16
     elif model_params['model_dtype'] == 'bfloat16':
@@ -52,23 +71,21 @@ def prepare_model_and_tokenizer(model_path, model_params, device):
     else: 
         dtype = torch.float32
     
-    model = AUTO_MODEL_CLS.from_pretrained(model_path, device_map=device, torch_dtype=dtype, attn_implementation='flash_attention_2' if model_params['use_flash_attn2'] else None)
+    model = AUTO_MODEL_CLS.from_pretrained(model_path,torch_dtype=dtype, attn_implementation='flash_attention_2' if model_params['use_flash_attn2'] else None)
     tokenizer = AutoTokenizer.from_pretrained(model_path)
-    
-    if tokenizer.chat_template is None:
-        model, tokenizer = setup_chat_format(model, tokenizer, resize_to_multiple_of=8)
-    
-    tokenizer.pad_token = tokenizer.eos_token
+    ## Note: Commenting out due to the fact it changes the embedding layer of the model
+    # if tokenizer.chat_template is None:
+    #  model, tokenizer = setup_chat_format(model, tokenizer, resize_to_multiple_of=8)
+    # tokenizer.padding_side = 'right'
 
-    tokenizer.padding_side = 'left'
-
-    if 'gemma' in model_path.lower():
-        tokenizer.add_bos_token = False
-        tokenizer.add_eos_token = True
+    # if 'gemma' in model_path.lower():
+    #     tokenizer.add_bos_token = False
+    #     tokenizer.add_eos_token = True
 
     if 'llama' in model_path.lower():
         tokenizer.add_bos_token = False
         tokenizer.add_eos_token = False
+        tokenizer.pad_token = tokenizer.eos_token
 
     if 'mistral' in model_path.lower():
         tokenizer.add_bos_token = False
@@ -77,39 +94,45 @@ def prepare_model_and_tokenizer(model_path, model_params, device):
     return model, tokenizer
 
 
-def run_mitigate_mode(args):
+def run_mitigate_mode(argv):
 
     # Validate config file against schema
-    with open(args.metaparameters_filepath) as config_file:
+    with open(argv.metaparameters_filepath) as config_file:
         config_json = json.load(config_file)
-    with open(args.schema_filepath) as schema_file:
+    with open(argv.schema_filepath) as schema_file:
         schema_json = json.load(schema_file)
 
     # Throws a fairly descriptive error if validation fails.
     jsonschema.validate(instance=config_json, schema=schema_json)
-
-    model, tokenizer = prepare_model_and_tokenizer(args.model, config_json['model_parameters'], args.device)
+    model, tokenizer = prepare_model_and_tokenizer(argv.model, config_json['model_parameters'])
     peft_config = prepare_peft(config_json['lora_parameters'])
-    dataset = prepare_dataset(args.dataset_dirpath)
+    dataset = prepare_dataset(argv.dataset_dirpath)
     print("Finished prepping dataset")
     print_gpu_utilization()
-    collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    ## This is for Llama:
+    # INSTRUCTION_TEMPLATE = '<|begin_of_text|><|start_header_id|>user<|end_header_id|>'
+    # RESPONSE_TEMPLATE = '<|start_header_id|>assistant<|end_header_id|>'
+    # INSTRUCTION_TEMPLATE = '<start_of_turn>user<end_of_turn>'
+    # RESPONSE_TEMPLATE = '<end_of_turn>model<end_of_turn>'
+    response = RESPONSE_TEMPLATE_LOOKUP[argv.model]
+    instruction = INSTRUCTION_TEMPLATE_LOOKUP[argv.model] 
+    collator = DataCollatorForCompletionOnlyLM(tokenizer=tokenizer, response_template=response, instruction_template=instruction)
 
-    mitigation = prepare_mitigation(config_json, args)
+    mitigation = prepare_mitigation(config_json, argv)
     mitigated_model = mitigation.mitigate_model(
         model=model,
         collator=collator,
         peft_config=peft_config,
         dataset=dataset
     )
-    mitigated_model.save_pretrained(args.output_dirpath)
+    mitigated_model.save_pretrained(argv.output_dirpath)
 
 
-def run_test_mode(args):
+def run_test_mode(argv):
     # Validate config file against schema
-    with open(args.metaparameters_filepath) as config_file:
+    with open(argv.metaparameters_filepath) as config_file:
         config_json = json.load(config_file)
-    with open(args.schema_filepath) as schema_file:
+    with open(argv.schema_filepath) as schema_file:
         schema_json = json.load(schema_file)
 
     # Throws a fairly descriptive error if validation fails.
@@ -122,16 +145,16 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
     
     parser = ArgumentParser(description='Parser for the LLM mitigation round with two modes of operation, mitigate and test')
-    parser.set_defaults(func=lambda args: parser.print_help())
+    parser.set_defaults(func=lambda argv: parser.print_help())
     subparser = parser.add_subparsers(dest='cmd', required=True)
 
     # Mitigation arguments
     mitigate_parser = subparser.add_parser('mitigate', help='Generates a mitigated model')
 
-    mitigate_parser.add_argument("--metaparameters_filepath", type=str, required=True, help="Path JSON file containing values of tunable parameters based on json schema")
-    mitigate_parser.add_argument("--schema_filepath", type=str, help="Path to a schema file in JSON Schema format against which to validate the metaparameters file.", required=True)
+    mitigate_parser.add_argument("--metaparameters_filepath","-j", type=str, required=True, help="Path JSON file containing values of tunable parameters based on json schema")
+    mitigate_parser.add_argument("--schema_filepath", "-s", type=str, help="Path to a schema file in JSON Schema format against which to validate the metaparameters file.", required=True)
     mitigate_parser.add_argument('--model', type=str, default="meta-llama/Meta-Llama-3-8B-Instruct", help="Huggingface model")
-    mitigate_parser.add_argument('--dataset_dirpath', type=str, help="A dataset of examples to train the mitigated model with.", required=True)
+    mitigate_parser.add_argument('--dataset_dirpath', "-d", type=str, help="A dataset of examples to train the mitigated model with.", required=True)
     mitigate_parser.add_argument('--output_dirpath', type=str, default="./out", help="The directory path to where the output will be dumped")
     mitigate_parser.add_argument('--scratch_dirpath', type=str, default="./scratch", help="The directory where a scratch space is located.")
     mitigate_parser.add_argument('--batch_size', type=int, default=32, help="The batch size that the technique would use for data loading")
@@ -142,10 +165,10 @@ if __name__ == "__main__":
     # Test arguments
     test_parser = subparser.add_parser('test', help='Tests a mitigated model with example data')
 
-    test_parser.add_argument("--metaparameters_filepath", type=str, required=True, help="Path JSON file containing values of tunable parameters based on json schema")
-    test_parser.add_argument("--schema_filepath", type=str, help="Path to a schema file in JSON Schema format against which to validate the metaparameters file.", required=True)
+    test_parser.add_argument("--metaparameters_filepath", "-j", type=str, required=True, help="Path JSON file containing values of tunable parameters based on json schema")
+    test_parser.add_argument("--schema_filepath", "-s", type=str, help="Path to a schema file in JSON Schema format against which to validate the metaparameters file.", required=True)
     test_parser.add_argument('--model', type=str, default="./model.pt", help="File path to the mitigated model that will be tested")
-    test_parser.add_argument('--dataset_dirpath', type=str, help="A dataset of examples to test the mitigated model with.", required=True)
+    test_parser.add_argument('--dataset_dirpath', "-d", type=str, help="A dataset of examples to test the mitigated model with.", required=True)
     test_parser.add_argument('--scratch_dirpath', type=str, default="./scratch", help="The directory where a scratch space is located.")
     test_parser.add_argument('--output_dirpath', type=str, default="./out", help="The directory path to where the output will be dumped")
     test_parser.add_argument('--batch_size', type=int, default=32, help="The batch size that the technique would use for data loading")
@@ -157,7 +180,7 @@ if __name__ == "__main__":
     mitigate_parser.set_defaults(func=run_mitigate_mode)
     test_parser.set_defaults(func=run_test_mode)
 
-    args = parser.parse_args()
+    argv = parser.parse_args()
 
     # Call appropriate function
-    args.func(args)
+    argv.func(argv)
--- trojai-llm-mitigation-framework/trojai_llm_mitigation_round/mitigations/finetuning.py
+++ trojai-llm-mitigation-framework/trojai_llm_mitigation_round/mitigations/finetuning.py
@@ -1,15 +1,30 @@
 from transformers import AutoModel, AutoTokenizer
 from peft import LoraConfig, get_peft_model
 from transformers import TrainingArguments, Trainer, DataCollatorForLanguageModeling
-from trl import SFTTrainer
+from trl import SFTTrainer, SFTConfig
 from datasets import Dataset as HF_Dataset
 from itertools import chain
 
-
+import datetime
 
 from .llm_mitigation import TrojAIMitigationLLM
 from ..utils import print_summary
 
+class debugOutput():
+    def __init__(self, tokenizer):
+        self.all_text = []
+        self.tokenizer = tokenizer
+
+    def __call__(self, output, compute_result=False):
+        # self.all_text.append(results)
+        if compute_result:
+            results = self.tokenizer.decode(output[0][0].argmax(axis=1))
+            output[1][0][output[1][0] == -100] = self.tokenizer.bos_token_id
+            label = self.tokenizer.decode(output[1][0])
+            # print(f"Output Prediction: {results} True Label: {label}")
+            return {'text_results': 1}
+        else:
+            return {'text_results':1}
 
 class FineTuningTrojaiMitigationLLM(TrojAIMitigationLLM):
     def __init__(self, lr, train_epochs, optim, device, batch_size=4, num_workers=1, bf16=False, **kwargs):
@@ -20,29 +35,37 @@ class FineTuningTrojaiMitigationLLM(TrojAIMitigationLLM):
 
     def mitigate_model(self,  model: AutoModel, collator: DataCollatorForLanguageModeling, peft_config: LoraConfig, dataset: HF_Dataset):
         target_token_length = collator.tokenizer.model_max_length
-
         # TODO: Expose as parameter, and experiment multi-GPU and memory consumption
         if target_token_length > self.max_token_length:
             target_token_length = self.max_token_length        
-
         peft_model = get_peft_model(model, peft_config)
-        training_args = TrainingArguments(output_dir="test_trainer",
+        now = datetime.datetime.now()
+        formatted  = now.strftime("%Y%m%d%H%M%S.%f")[:-3]
+        training_args = SFTConfig(output_dir=f"test_trainer_{formatted}",
                                             learning_rate=self.lr,
                                             num_train_epochs=self.train_epochs,
                                             optim=self.optim,
-                                            evaluation_strategy="epoch", 
+                                            eval_strategy="epoch",
+                                            save_strategy="epoch",
+                                            # eval_steps=10,
                                             per_device_train_batch_size=self.batch_size,
                                             per_device_eval_batch_size=self.batch_size,
                                             bf16=self.bf16,
                                             logging_strategy="epoch",
-                                            report_to="tensorboard")
+                                            report_to="tensorboard",
+                                            max_seq_length = target_token_length,
+                                            batch_eval_metrics=True,
+                                            )
+
+        debug_output = debugOutput(collator.tokenizer)
+        
         trainer = SFTTrainer(
             model=peft_model,
             args=training_args,
             train_dataset=dataset["train"],
             eval_dataset=dataset["test"],
             data_collator=collator,
-            max_seq_length=self.max_token_length
+            # compute_metrics=debug_output,
         )
         
         print(f"Beginning Training with {len(dataset['train'])} examples")
